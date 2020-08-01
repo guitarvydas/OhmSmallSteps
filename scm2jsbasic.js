da2ce3f8a6949d1b761956632188db1cd180a9f8
Paul Tarvydas <guitarvydas@gmail.com>
2020-08-01 06:55:34 -0400

    scm2jsbasic

---
+++ scm2jsbasic.js
@@ -0,0 +1,16 @@
+const fs = require ('fs')
+const ohm = require ('ohm-js')
+const grammarData = fs.readFileSync('scm2jsbasic.ohm')
+const grammar = ohm.grammar(grammarData)
+
+const input = fs.readFileSync('prolog-6.scm')
+
+const result = grammar.match(input)
+// console.log(grammar.trace(input).toString())
+
+if (result.succeeded()) {
+    console.log("Matching Succeeded")
+} else {
+    console.log("Matching Failed")
+}
+
