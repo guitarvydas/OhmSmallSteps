ac6711d9bcc6a8a581493e7abb00d449066bd79d
Paul Tarvydas <guitarvydas@gmail.com>
2020-08-01 06:15:07 -0400

    scm2jsbasic1.js

--- scm2jsbasic1.js
+++ scm2jsbasic1.js
@@ -1,14 +1,14 @@
 // usage:
 // npm install ohm-js
-// node scm2js.js
+// node scm2jsbasic1.js
 
 
 const fs = require ('fs')
 const ohm = require ('ohm-js')
-const grammarData = fs.readFileSync('scm2jsbasic.ohm')
+const grammarData = fs.readFileSync('scm2jsbasic1.ohm')
 const grammar = ohm.grammar(grammarData)
 
-const input = fs.readFileSync('scm2jstest.scm')
+const input = fs.readFileSync('scm2jstest1.scm')
 
 const result = grammar.match(input)
 
