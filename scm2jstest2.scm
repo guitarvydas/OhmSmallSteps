623a97cd62b52fa7775ceeb160f9f4642583b03d
Paul Tarvydas <guitarvydas@gmail.com>
2020-07-31 20:58:04 -0400

    fixes to scm2jsbasic2

--- scm2jstest2.scm
+++ scm2jstest2.scm
@@ -1,3 +1,10 @@
 (define (clear_r x)
   (set-car! (cddr x) '(()))
   (append (cdr a) `(r! ,l) (cdr g)))
+
+(define (clear_r x)
+  (set-car! (cddr x) '(()))
+  (append (cdr a) `(r! ,l) (cdr g)))
+
+(a b . c)
+((a b . c))
