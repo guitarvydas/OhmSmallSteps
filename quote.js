5f1ae5b9bf53ea28c47bad19c4af83ff541ae6ad
Paul Tarvydas <guitarvydas@gmail.com>
2020-08-03 17:48:15 -0400

    quote dissection

---
+++ quote.js
@@ -0,0 +1,35 @@
+const fs = require ('fs')
+const ohm = require ('ohm-js')
+const grammarData = fs.readFileSync('quote.ohm')
+const grammar = ohm.grammar(grammarData)
+
+const input = fs.readFileSync('quote.scm')
+const input_rewritten = fs.readFileSync('quote_rewritten.scm')
+
+const result = grammar.match(input)
+
+if (result.succeeded()) {
+    console.log("Matching Succeeded")
+} else {
+    console.log("Matching Failed")
+}
+
+const result_rewritten = grammar.match(input_rewritten)
+
+if (result_rewritten.succeeded()) {
+    console.log("Matching Rewrite Succeeded")
+} else {
+    console.log("Matching Rewrite Failed")
+}
+
+
+console.log()
+console.log("result cst")
+console.log(result._cst)
+console.log()
+
+console.log()
+console.log("result_rewritten cst")
+console.log(result_rewritten._cst)
+console.log()
+
