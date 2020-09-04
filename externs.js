8fb90a6b59fba9611dea105692e4599b469ab6f1
Paul Tarvydas <guitarvydas@gmail.com>
2020-09-04 16:03:08 -0400

    support functions that will be need (in extern.js)

---
+++ externs.js
@@ -0,0 +1,10 @@
+function list() {};
+function car() {};
+function cadr() {};
+function caddr() {};
+function cadddr() {};
+
+function eq_Q_() {};
+function null_Q_() {};
+function pair_Q_() {};
+function string_eq() {};
