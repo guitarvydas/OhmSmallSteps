4d8a38b3fe2c9df444b992fc299ab3577540c1f4
Paul Tarvydas <guitarvydas@gmail.com>
2020-09-11 21:54:51 -0400

    wip: working through support.js

--- prolog-6.js
+++ prolog-6.js
@@ -95,9 +95,6 @@ function pair_Q_() {};
 function string_eq() {};
 */
 
-
-
-
 function __try(g,r,e,n) {return (function(){ if (null_Q_(r)) {return false;} else {return (function(a=copy(car(r),list(n)),ne=unify(car(g),car(a),e)) {(function(){ if (ne) {return prove3(append(cdr(a),cdr(g)),ne,_plus(1,n));} else { return null;}})();return __try(g,cdr(r),e,n);})();}})();};
 function prove3(g,e,n) {return (function(){if (null_Q_(g)) {return print_frame(e);}else {return __try(g,db,e,n);}})();};
 var link = list;
@@ -109,10 +106,10 @@ function L_n(x) {return car(cddddr(x));};
 function back5(l,g,r,e,n) {return (function(){ if (pair_Q_(g) && pair_Q_(r)) {return prove5(l,g,cdr(r),e,n);} else {return prove5(L_l(l),L_g(l),cdr(L_r(l)),L_e(l),L_n(l));}})();};
 function prove5(l,g,r,e,n) {return (function(){if (null_Q_(g)) {print_frame(e);return back5(l,g,r,e,n);} else if (null_Q_(r)) {return (function(){ if (null_Q_(l)) {return true;} else {return back5(l,g,r,e,n);}})();}else {return (function(a=copy(car(r),n),e_A_=unify(car(a),car(g),e)) {return (function(){ if (e_A_) {return prove5(link(l,g,r,e,n),append(cdr(a),cdr(g)),db,e_A_,_plus(1,n));} else {return back5(l,g,r,e,n);}})();})();}})();};
 function L_c(x) {return cadr(cddddr(x));};
-function clear_r(x) {return set-car_B_(cddr(x),new List(new List()));};
+function clear_r(x) {return set-car_B_(cddr(x),new Cons(new Cons()));};
 function back6(l,g,r,e,n,c) {return (function(){if (pair_Q_(g) && pair_Q_(r)) {return prove6(l,g,cdr(r),e,n,c);} else if (pair_Q_(l)) {return prove6(L_l(l),L_g(l),cdr(L_r(l)),L_e(l),L_n(l),L_c(l));} else { return null; }})();};
 function prove6(l,g,r,e,n,c) {return (function(){if (null_Q_(g)) {print_frame(e);return back6(l,g,r,e,n,c);} else if (eq_Q_("!",car(g))) {clear_r(c);return prove6(c,cdr(g),r,e,n,c);} else if (eq_Q_("r!",car(g))) {return prove6(l,cddr(g),r,e,n,cadr(g));} else if (null_Q_(r)) {return (function(){ if (null_Q_(l)) {return true;} else {return back6(l,g,r,e,n,c);}})();}else {return (function(a=copy(car(r),n),e_A_=unify(car(a),car(g),e)) {return (function(){ if (e_A_) {return prove6(link(l,g,r,e,n,c),append(cdr(a),list("r!",l),cdr(g)),db,e_A_,_plus(1,n),l);} else {return back6(l,g,r,e,n,c);}})();})();}})();};
-var empty = new List(new List("bottom"));
+var empty = new Cons(new Cons("bottom"));
 var name = cadr;
 var time = cddr;
 function var_Q_(x) {return pair_Q_(x) && eq_Q_("?",car(x));};
@@ -125,10 +122,10 @@ function unify(x1,y1,e) {return (function(x=value(x1,e),y=value(y1,e)) {return (
 function resolve(x,e) {return (function(){if ((!pair_Q_(x))) {x} else if (var_Q_(x)) {return (function(v=value(x,e)) {return (function(){ if (var_Q_(v)) {return v;} else {return resolve(v,e);}})();})();}else {return cons(resolve(car(x),e),resolve(cdr(x),e));}})();};
 function print_frame_loop(ee) {return (function(){ if (pair_Q_(cdr(ee))) {return (function(_xx=0) {(function(){ if (null_Q_(time(caar(ee)))) {return (function(_yy=0) {display(cadaar(ee)); display(" = "); display(resolve(caar(ee),e));return neline();})();} else { return null;}})();return print_frame_loop(cdr(ee));})();} else { return null;}})();};
 function print_frame(e) {newline();return print_frame_loop(e);};
-var db = new List(new List(new List("edge","a","b")),new List(new List("edge","a","f")),new List(new List("edge","a","g")),new List(new List("edge","b","c")),new List(new List("edge","b","d")),new List(new List("edge","c","d")),new List(new List("edge","c","e")),new List(new List("edge","g","h")),new List(new List("edge","d","h")),new List(new List("edge","h","e")),new List(new List("edge","h","f")),new List(new List("path",new List("?","A"),new List("?","B"),new List(new List("?","A"),new List("?","B"))),new List("edge",new List("?","A"),new List("?","B"))),new List(new List("path",new List("?","A"),new List("?","B"),new Pair(new List("?","A"),new List("?","CB"))),new List("edge",new List("?","A"),new List("?","C")),new List("path",new List("?","C"),new List("?","B"),new List("?","CB"))));
-var goals = new List(new List("path","a","f",new List("?","P")));
+var db = new Cons(new Cons(new Cons("edge","a","b")),new Cons(new Cons("edge","a","f")),new Cons(new Cons("edge","a","g")),new Cons(new Cons("edge","b","c")),new Cons(new Cons("edge","b","d")),new Cons(new Cons("edge","c","d")),new Cons(new Cons("edge","c","e")),new Cons(new Cons("edge","g","h")),new Cons(new Cons("edge","d","h")),new Cons(new Cons("edge","h","e")),new Cons(new Cons("edge","h","f")),new Cons(new Cons("path",new Cons("?","A"),new Cons("?","B"),new Cons(new Cons("?","A"),new Cons("?","B"))),new Cons("edge",new Cons("?","A"),new Cons("?","B"))),new Cons(new Cons("path",new Cons("?","A"),new Cons("?","B"),new Cons(new Cons("?","A"),new Cons("?","CB"))),new Cons("edge",new Cons("?","A"),new Cons("?","C")),new Cons("path",new Cons("?","C"),new Cons("?","B"),new Cons("?","CB"))));
+var goals = new Cons(new Cons("path","a","f",new Cons("?","P")));
 prove3(goals,empty,1);
-prove5(new List(),goals,db,empty,1);
-var db = new List(new List(new List("some","foo")),new List(new List("some","bar")),new List(new List("some","baz")),new List(new List("eq",new List("?","X"),new List("?","X"))),new List(new List("neq",new List("?","X"),new List("?","Y")),new List("eq",new List("?","X"),new List("?","Y")),"!","fail"),new List(new List("neq",new List("?","X"),new List("?","Y"))));
-var goals = new List(new List("some",new List("?","X")),new List("some",new List("?","Y")),new List("neq",new List("?","X"),new List("?","Y")));
-prove6(new List(),goals,db,empty,1,new List());
+prove5(new Cons(),goals,db,empty,1);
+var db = new Cons(new Cons(new Cons("some","foo")),new Cons(new Cons("some","bar")),new Cons(new Cons("some","baz")),new Cons(new Cons("eq",new Cons("?","X"),new Cons("?","X"))),new Cons(new Cons("neq",new Cons("?","X"),new Cons("?","Y")),new Cons("eq",new Cons("?","X"),new Cons("?","Y")),"!","fail"),new Cons(new Cons("neq",new Cons("?","X"),new Cons("?","Y"))));
+var goals = new Cons(new Cons("some",new Cons("?","X")),new Cons("some",new Cons("?","Y")),new Cons("neq",new Cons("?","X"),new Cons("?","Y")));
+prove6(new Cons(),goals,db,empty,1,new Cons());
